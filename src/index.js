35b1b6ed660ab3755849c70189240b0eff709cf0
Danil Kohan <Sportodavka@gmail.com>
2020-10-20 20:48:42 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let rev = n;
+   if (n < 0) {
+    rev = n * (-1);
+   } 
+   rev = Number(String(rev).split("").reverse().join(""));
+   return rev;
 }
